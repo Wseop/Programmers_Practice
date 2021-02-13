b3cce852f0f8e928a9e77a76698896315a961e3d
Wseop <crazyz2000@naver.com>
2021-02-13 17:50:56 +0900

    add comments - Full_Search

--- Full_Search/Carpet.cpp
+++ Full_Search/Carpet.cpp
@@ -5,12 +5,15 @@ using namespace std;
 
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
-    int x, y;
+    int x, y;   //예상되는 가로, 세로 길이를 위한 변수
     
+    //brown 값을 기준으로 가능한 가로, 세로의 길이를 모두 탐색
     for (x = (brown / 2) - 1; x >= 0; x--) {
         int predict_y = 0;
         
         y = ((brown - (x * 2)) / 2) + 2;
+        
+        //예측한 가로, 세로길이로 yellow를 만들 수 있는지 확인
         predict_y = (x - 2) * (y - 2);
         if (predict_y == yellow) {
             break;
