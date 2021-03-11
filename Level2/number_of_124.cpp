8ca4a944f22029c94a41326eeb92425937ea2ff0
Wseop <crazyz2000@naver.com>
2021-03-11 15:14:32 +0900

    edit number_of_124 - some condition checked twice

--- Level2/number_of_124.cpp
+++ Level2/number_of_124.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 // 1, 2, 4를 순서대로 사용하는 방식 1 -> 2 -> 4 -> 11 -> 12 -> 14 -> ...
 // 3의 배수가 아닌 경우는 3으로 나눈 나머지 (0인 경우는 4)
-// 3의 배수인 경우는 결과에 4를 붙이고 3으로 나눈 몫의 -1에 대해 위 방식 적용
+// 만약 3의 배수라면 1을 추가로 빼줌
 string solution(int n) {
     string answer = "";
     string answer_reverse = "";
@@ -18,7 +18,8 @@ string solution(int n) {
             answer_reverse += '4';
             n = (n / 3) - 1;
         } else {
-            answer_reverse += n % 3 == 0 ? '4' : ((n % 3) + '0');
+            //answer_reverse += n % 3 == 0 ? '4' : ((n % 3) + '0');
+            answer_reverse += (n % 3) + '0';
             n /= 3;
         }
     }
