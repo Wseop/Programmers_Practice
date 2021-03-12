48bf17a3b655d0227c2b6fdde90de59a1a166cd0
Wseop <crazyz2000@naver.com>
2021-03-12 15:43:24 +0900

    edit exception case

--- Level2/string_compression.cpp
+++ Level2/string_compression.cpp
@@ -6,37 +6,42 @@
 
 using namespace std;
 
-// Need to edit!!!
 int solution(string s) {
     int answer = s.size();
-    
-    for (int cut_size = 1; cut_size <= s.size(); cut_size++) {
+
+    // 압축 가능한 문자열의 길이가 전체 문자열 크기의 절반을 넘어서면 압축 불가
+    // 따라서, cut_size = 1 ~ (문자열 길이 / 2)
+    for (int cut_size = 1; cut_size <= s.size() / 2; cut_size++) {
         string new_str = "";
-        string cut_string = s.substr(0, cut_size);
-        int count = 0;
+        string cut_string = s.substr(0, cut_size);      // 압축할 문자열
+        int count = 0;                                  // 압축 가능한 문자열 갯수
+        int index = 0;                                  // substr을 추출할 시작 위치
         
-        for (int index = 0; index < s.size();) {
+        while (true) {
             string sub_s;
             
-            if (s.size() - index < cut_size) {
-                new_str += cut_string;
+            // case 1. index가 size를 넘어서는 경우는 문자열의 길이에 딱 맞게 종료된 경우 -> count + 압축할 문자열을 붙인 후 종료
+            // case 2. size를 초과하여 cut_size크기의 substr을 생성할 수 없는 경우 -> 남은 문자열까지 추가로 붙인 후 종료
+            if (index > s.size()) {
+                new_str += (count == 1) ? cut_string : to_string(count) + cut_string;
+                break;
+            } else if (index + cut_size > s.size()) {
+                new_str += (count == 1) ? cut_string : to_string(count) + cut_string;
                 new_str += s.substr(index, s.size() - index);
                 break;
             }
+            
             sub_s = s.substr(index, cut_size);
-            if (sub_s == cut_string) {
+            if (sub_s == cut_string) {    
                 count++;
                 index += cut_size;
-                if (index == s.size()) 
-                    new_str += (count == 1) ? cut_string : (to_string(count) + cut_string);
-            } else {
-                new_str += (count == 1) ? cut_string : (to_string(count) + cut_string);
+            } else {                       
+                new_str += (count == 1) ? cut_string : to_string(count) + cut_string;
                 count = 0;
                 cut_string = sub_s;
             }
         }
-        //cout << new_str << endl;
-        answer = (new_str.size() < answer) ? new_str.size() : answer;
+        answer = (answer > new_str.size()) ? new_str.size() : answer;
     }
     
     return answer;
